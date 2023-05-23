e9c5479d2269ab489e540cbfc51ab4ec212716e7
Nikko <51402422+niikocap@users.noreply.github.com>
2023-05-23 13:46:17 +0800

    Add files via upload

---
+++ dollars.php
@@ -0,0 +1,10 @@
+<?php
+//$sn = "localhost";
+//$pw = "sg*&KlRcx(kAu^#1";
+//$un = "id20710488_spotmiidb";
+//$db = "id20710488_spotmii";
+$sn = "localhost";
+$pw = "A4jtiasiC-y4Rm7";
+$un = "521511";
+$db = "521511";
+?>
